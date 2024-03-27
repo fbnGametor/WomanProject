9de36dabd4df80b8677659eaf8f0b3f76aaeaf96
Fabio Amancio <trindadefabio2016@gmail.com>
2024-03-27 17:05:57 -0300

    Sistema de gravacao de arquivo com o rank terminado

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,6 +32,7 @@ public class Program {
         printRanks(mulheres, homens, people);
 
         while(true) {
+
             System.out.println("\n\n");
             System.out.print("Deseja avaliar mais pessoas (s/n): ");
             String sCheck = sc.next().strip().toLowerCase();
@@ -51,86 +52,111 @@ public class Program {
             } else if (cCheck == 'n') {
                 break;
             } else {
-                throw new AvaliatorException("Confirmacao de repeticao invalida!");
+                AvaliatorException e = new AvaliatorException("Confirmacao de repeticao invalida!");
+                System.out.println("Error: " + e.getMessage());
             }
         }
 
-        writeFile(mulheres, homens, people, sc);
+        try {
+            System.out.println("Deseja salvar o rank (s/n)? ");
+            String sCheck = sc.next().strip().toLowerCase();
+            char check = sCheck.charAt(0);
+
+            if(check == 's') {
+                writeFile(mulheres, homens, people, sc);
+            }
+        } catch(InputMismatchException e) {
+            AvaliatorException a = new AvaliatorException("InputMismatchException error");
+            System.out.println("Error: " + a.getMessage());
+        }
+
         endOfProgram();
 
         sc.close();
     }
 
     private static Woman womanScan(Scanner sc) {
-        System.out.print("Nome: ");
-        sc.nextLine();
-        String name = sc.nextLine();
-        System.out.print("Idade: ");
-        int age = sc.nextInt();
-        System.out.print("Altura [ cm ]: ");
-        int height = sc.nextInt();
-        System.out.print("Peso: ");
-        double weight = sc.nextDouble();
-        System.out.println("Tipos de cabelos: ");
-        System.out.println("[ 1 ] Careca\n[ 2 ] Muito Curto\n[ 3 ] Curto\n[ 4 ] Medio\n[ 5 ] Longo\n[ 6 ] Passando Da Bunda");
-        System.out.print("Tamanho do cabelo: ");
-        int hairLenght = sc.nextInt();
-        System.out.println("Cores de cabelo: ");
-        System.out.println("PRETO | CASTANHO\nLOIRO | RUIVO\nBRANCO | CINZA\nAZUL | VERDE\nLUZES | FELIPE_NETO\nSEM_CABELO");
-        System.out.print("Cor do cabelo: ");
-        HairColor hairColor = HairColor.valueOf(sc.next().strip().toUpperCase());
-        System.out.println("Cor dos olhos:");
-        System.out.println("CASTANHO\nAZUL\nVERDE\nHAZEL");
-        System.out.print("Cor dos olhos: ");
-        EyeColor eyeColor = EyeColor.valueOf(sc.next().strip().toUpperCase());
-        System.out.println("Tipo do corpo: ");
-        System.out.println("[ 1 ] Thais Carla\n[ 2 ] Magerrima\n[ 3 ] Magra\n[ 4 ] Gorda\n[ 5 ] Normal\n[ 6 ] Gostosa\n[ 7 ] Nave");
-        System.out.print("Corpo: ");
-        int hotness = sc.nextInt();
-        System.out.println("Tem algum problema listado abaixo:");
-        System.out.println("[ 1 ] Normal\n[ 2 ] Autismo\n[ 3 ] Sindrome de Down\n[ 4 ] Nanismo\n[ 5 ] Leandrinha vibes\n[ 6 ] Psicopatia");
-        System.out.print("Tem problema: ");
-        int problem = sc.nextInt();
-        System.out.println("Mulher adcionada na lista de comparacao");
+        Woman woman = null;
+        try {
+            System.out.print("Nome: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Idade: ");
+            int age = sc.nextInt();
+            System.out.print("Altura [ cm ]: ");
+            int height = sc.nextInt();
+            System.out.print("Peso: ");
+            double weight = sc.nextDouble();
+            System.out.println("Tipos de cabelos: ");
+            System.out.println("[ 1 ] Careca\n[ 2 ] Muito Curto\n[ 3 ] Curto\n[ 4 ] Medio\n[ 5 ] Longo\n[ 6 ] Passando Da Bunda");
+            System.out.print("Tamanho do cabelo: ");
+            int hairLenght = sc.nextInt();
+            System.out.println("Cores de cabelo: ");
+            System.out.println("PRETO | CASTANHO\nLOIRO | RUIVO\nBRANCO | CINZA\nAZUL | VERDE\nLUZES | FELIPE_NETO\nSEM_CABELO");
+            System.out.print("Cor do cabelo: ");
+            HairColor hairColor = HairColor.valueOf(sc.next().strip().toUpperCase());
+            System.out.println("Cor dos olhos:");
+            System.out.println("CASTANHO\nAZUL\nVERDE\nHAZEL");
+            System.out.print("Cor dos olhos: ");
+            EyeColor eyeColor = EyeColor.valueOf(sc.next().strip().toUpperCase());
+            System.out.println("Tipo do corpo: ");
+            System.out.println("[ 1 ] Thais Carla\n[ 2 ] Magerrima\n[ 3 ] Magra\n[ 4 ] Gorda\n[ 5 ] Normal\n[ 6 ] Gostosa\n[ 7 ] Nave");
+            System.out.print("Corpo: ");
+            int hotness = sc.nextInt();
+            System.out.println("Tem algum problema listado abaixo:");
+            System.out.println("[ 1 ] Normal\n[ 2 ] Autismo\n[ 3 ] Sindrome de Down\n[ 4 ] Nanismo\n[ 5 ] Leandrinha vibes\n[ 6 ] Psicopatia");
+            System.out.print("Tem problema: ");
+            int problem = sc.nextInt();
+            System.out.println("Mulher adcionada na lista de comparacao");
 
-        return new Woman(name, age, height, weight, hairLenght, hairColor, eyeColor, hotness, problem);
+            woman = new Woman(name, age, height, weight, hairLenght, hairColor, eyeColor, hotness, problem);
+        } catch(InputMismatchException | IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return woman;
     }
 
     private static Man manScan(Scanner sc) {
-        System.out.print("Nome: ");
-        sc.nextLine();
-        String name = sc.nextLine();
-        System.out.print("Idade: ");
-        int age = sc.nextInt();
-        System.out.print("Altura [ cm ]: ");
-        int height = sc.nextInt();
-        System.out.print("Peso: ");
-        double weight = sc.nextDouble();
-        System.out.println("Tipos de cabelos: ");
-        System.out.println("[ 1 ] Careca | [ 2 ] Buzz Cut\n[ 3 ] Corte Social | [ 4 ] Americano\n[ 5 ] Low Fade | [ 6 ] Mid Fade\n[ 7 ] High Fade | [ 8 ] Mullet\n[ 9 ] Moica Trem | [ 10 ] Medio\n[ 11 ] Longo | [ 12 ] Tigelinha");
-        System.out.print("Estilo do cabelo: ");
-        int hairLenght = sc.nextInt();
-        System.out.println("Cores de cabelo: ");
-        System.out.println("PRETO | CASTANHO\nLOIRO | RUIVO\nBRANCO | CINZA\nAZUL | VERDE\nLUZES | FELIPE_NETO\nSEM_CABELO");
-        System.out.print("Cor do cabelo: ");
-        HairColor hairColor = HairColor.valueOf(sc.next().toUpperCase());
-        System.out.println("Cor dos olhos:");
-        System.out.println("CASTANHO | AZUL\nVERDE | HAZEL");
-        System.out.print("Cor dos olhos: ");
-        EyeColor eyeColor = EyeColor.valueOf(sc.next().toUpperCase());
-        System.out.println("Tipo do corpo: ");
-        System.out.println("[ 1 ] Thaiso Carlo\n[ 2 ] Magrelo | [ 3 ] Magro\n[ 4 ] Gordo | [ 5 ] Gordinho\n[ 6 ] Normal | [ 7 ] Fortinho\n[ 8 ] Sheipado | [ 9 ] Fortasso\n[ 10 ] CBum");
-        System.out.print("Corpo: ");
-        int body = sc.nextInt();
-        System.out.print("Qual o salario: R$ ");
-        double balance = sc.nextDouble();
-        System.out.println("Tem algum problema listado abaixo:");
-        System.out.println("[ 1 ] Normal\n[ 2 ] Autismo\n[ 3 ] Sindrome de Down\n[ 4 ] Nanismo\n[ 5 ] Leandrinha vibes\n[ 6 ] Psicopatia");
-        System.out.print("Tem problema: ");
-        int problem = sc.nextInt();
-        System.out.println("Homem adcionado na lista de comparacao");
+        Man man = null;
+        try {
+            System.out.print("Nome: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Idade: ");
+            int age = sc.nextInt();
+            System.out.print("Altura [ cm ]: ");
+            int height = sc.nextInt();
+            System.out.print("Peso: ");
+            double weight = sc.nextDouble();
+            System.out.println("Tipos de cabelos: ");
+            System.out.println("[ 1 ] Careca | [ 2 ] Buzz Cut\n[ 3 ] Corte Social | [ 4 ] Americano\n[ 5 ] Low Fade | [ 6 ] Mid Fade\n[ 7 ] High Fade | [ 8 ] Mullet\n[ 9 ] Moica Trem | [ 10 ] Medio\n[ 11 ] Longo | [ 12 ] Tigelinha");
+            System.out.print("Estilo do cabelo: ");
+            int hairLenght = sc.nextInt();
+            System.out.println("Cores de cabelo: ");
+            System.out.println("PRETO | CASTANHO\nLOIRO | RUIVO\nBRANCO | CINZA\nAZUL | VERDE\nLUZES | FELIPE_NETO\nSEM_CABELO");
+            System.out.print("Cor do cabelo: ");
+            HairColor hairColor = HairColor.valueOf(sc.next().toUpperCase());
+            System.out.println("Cor dos olhos:");
+            System.out.println("CASTANHO | AZUL\nVERDE | HAZEL");
+            System.out.print("Cor dos olhos: ");
+            EyeColor eyeColor = EyeColor.valueOf(sc.next().toUpperCase());
+            System.out.println("Tipo do corpo: ");
+            System.out.println("[ 1 ] Thaiso Carlo\n[ 2 ] Magrelo | [ 3 ] Magro\n[ 4 ] Gordo | [ 5 ] Gordinho\n[ 6 ] Normal | [ 7 ] Fortinho\n[ 8 ] Sheipado | [ 9 ] Fortasso\n[ 10 ] CBum");
+            System.out.print("Corpo: ");
+            int body = sc.nextInt();
+            System.out.print("Qual o salario: R$ ");
+            double balance = sc.nextDouble();
+            System.out.println("Tem algum problema listado abaixo:");
+            System.out.println("[ 1 ] Normal\n[ 2 ] Autismo\n[ 3 ] Sindrome de Down\n[ 4 ] Nanismo\n[ 5 ] Leandrinha vibes\n[ 6 ] Psicopatia");
+            System.out.print("Tem problema: ");
+            int problem = sc.nextInt();
+            System.out.println("Homem adcionado na lista de comparacao");
 
-        return new Man(name, age, height, weight, hairLenght, hairColor, eyeColor, body, balance, problem);
+            man = new Man(name, age, height, weight, hairLenght, hairColor, eyeColor, body, balance, problem);
+        } catch(InputMismatchException | IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return man;
     }
 
     private static void rankeamentoLista(List list) {
@@ -148,6 +174,8 @@ public class Program {
         System.out.println("|************************|");
         System.out.println("      End of Program");
         System.out.println("|************************|");
+        System.out.println("\n\n");
+        System.out.println("Obrigado por utilizar o avaliador de pessoas");
     }
 
     private static void avaliadorPessoa(List mulheres, List homens, List people, Scanner sc) {
@@ -215,7 +243,7 @@ public class Program {
         sc.nextLine();
         String path = sc.nextLine();
         System.out.print("Quer chama-lo como: ");
-        String fileName = sc.next();
+        String fileName = sc.nextLine();
 
         path += "\\" + fileName + ".txt";
 
@@ -271,7 +299,7 @@ public class Program {
             bw.write("|***********************************************|");
 
         } catch(IOException e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
         }
      }
 }
